3e60d661d69caea351c81a101b530c745814f628
Lucian Codes <ayomideayomipo147@gmail.com>
2025-07-31 14:46:02 +0100

    1

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,7 +1,7 @@
 import knex from 'knex'
 import config from '../knexfile'
 
-const environment = process.env.NODE_ENV || 'development'
+const environment = process.env.NODE_ENV || 'production'
 const db = knex(config[environment])
 
 export default db
